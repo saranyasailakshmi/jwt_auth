a17116889167c7b27bda7f87fa057b1570090cb4
saranyasailakshmi <130599541+saranyasailakshmi@users.noreply.github.com>
2025-04-09 04:52:00 +0530

    Update urls.py

--- assignment/product/urls.py
+++ assignment/product/urls.py
@@ -7,9 +7,9 @@ from .views import (
 )
 
 urlpatterns=[
-    path('create/', ProductCreateAPIView.as_view(), name='create-product'),          # POST
-    path('products/', ProductListAPIView.as_view(), name='list-products'),             # GET
-    path('update/<int:pk>/', ProductUpdateAPIView.as_view(), name='update-product'),  # PUT
-    path('delete/<int:pk>/', ProductDeleteAPIView.as_view(), name='delete-product'),  # DELETE
+    path('create/', ProductCreateAPIView.as_view(), name='create-product'),          
+    path('products/', ProductListAPIView.as_view(), name='list-products'),             
+    path('update/<int:pk>/', ProductUpdateAPIView.as_view(), name='update-product'),  
+    path('delete/<int:pk>/', ProductDeleteAPIView.as_view(), name='delete-product'),  
 
 ]
